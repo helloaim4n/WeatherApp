a6826c19b35d8a8af9350e69e176164e336eef60
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2024-01-03 00:21:32 +0800

    Add Weather component for fetching and displaying weather data

---
+++ src/components/Weather.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+function Weather() {
+    const [city, setCity] = useState('');
+    const [weatherData, setWeatherData] = useState(null);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_WEATHER_API_KEY}`
+            );
+            setWeatherData(response.data);
+            console.log(response.data); 
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleInputChange = (e) => {
+        setCity(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(city) {
+            fetchData();
+        } else {
+            console.log("City name is required");
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter city name" value={city} onChange={handleInputChange} />
+                <button type="submit">Get The Weather</button>
+            </form>
+            {weatherData ? (
+                <>
+                    <h2>{weatherData.name}</h2>
+                    <p>Temperature: {weatherData.main.temp}°C</p>
+                    <p>Description: {weatherData.weather[0].description}</p>
+                    <p>Feels like: {weatherData.main.feels_like}°C</p>
+                    <p>Humidity: {weatherData.main.humidity}%</p>
+                    <p>Pressure: {weatherData.main.pressure}</p>
+                    <p>Wind Speed: {weatherData.wind.speed}</p>
+                </>
+            ) : (
+                <p>Loading weather data . . .</p>
+            )}
+        </div>
+    )
+}
+
+export default Weather;
